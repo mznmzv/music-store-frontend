e3936a0b2daa8efd0d63a315a144518e4c650c4a
bokonure <mazanmuz13@mail.ru>
2024-06-15 12:37:44 +0300

    changed axios url source

--- src/components/DealItem.jsx
+++ src/components/DealItem.jsx
@@ -1,4 +1,5 @@
 export const DealItem = ({ type, deal }) => {
+    console.log(deal)
     return (
         <div className='mx-auto justify-between'>
             <div className='bg-white p-5 flex flex-col rounded-xl h-full justify-between shadow-lg'>
@@ -40,26 +41,3 @@ export const DealItem = ({ type, deal }) => {
         </div>
     )
 }
-
-// const isOrdered = release.buyer !== ''
-// const onSell = release.price !== ''
-// const isMarket = window.location.pathname === '/market'
-
-//         <img src={`http://localhost:5000/${release.cover}`} alt='cover' />
-//         <div className='space-y-2 mt-3 text-sm'>
-//             {!isMarket && (
-//                 <>
-//                     {isOrdered && (
-//                         <span className='bg-lime-400 p-1 text-center block max-w-[100px] ml-auto rounded-md'>
-//                             Заказано
-//                         </span>
-//                     )}
-//                     {onSell && !isOrdered && (
-//                         <span className='bg-lime-400 p-1 text-center block max-w-[100px] ml-auto rounded-md'>
-//                             Продается
-//                         </span>
-//                     )}
-//                 </>
-//             )}
-//         </div>
-//     </div>
--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const instance = axios.create({ baseURL: 'http://localhost:5000/api' })
+const instance = axios.create({ baseURL: import.meta.env.VITE_API_URL })
 
 instance.interceptors.request.use(config => {
     config.headers.Authorization = window.localStorage.getItem('token')
